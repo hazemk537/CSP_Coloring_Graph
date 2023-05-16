fbfcee1721281397292360769f1d4ea045d6ab47
HazemShehawy <Hazemk537@gmail.com>
2023-05-16 09:12:50 +0300

    Adding Map Link

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from solver import *
 # Define the problem
 # Define the variables (regions) and their possible values (colors)
 variables = ['WA', 'NT', 'SA', 'Q', 'NSW', 'V'] #Australia Regions
+#https://2.bp.blogspot.com/-9mXaXVUbPHQ/T8CYBASTQ4I/AAAAAAAACIo/cW09mus4IEg/s760/australia-map-picture.gif
 domains = ['r', 'g', 'b']
 constraints=[
  ('WA', 'NT'),
