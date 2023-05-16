a035fe4ad03443c41cac0bdbb9282e1f55220741
HazemShehawy <Hazemk537@gmail.com>
2023-05-16 08:33:51 +0300

    solve Conflict

--- index.py
+++ index.py
@@ -1,30 +1,25 @@
-from index import *
-
+from solver import *
 # Define the problem
-
 # Define the variables (regions) and their possible values (colors)
-variables = ['WA', 'NT', 'SA', 'Q', 'NSW', 'V']
-values = ['r', 'g', 'b']
+variables = ['WA', 'NT', 'SA', 'Q', 'NSW', 'V'] #Australia Regions
+domains = ['r', 'g', 'b']
+constraints=[
+ ('WA', 'NT'),
+ ('WA', 'SA'),
+ ('NT', 'SA'),
+ ('NT', 'Q'),
+ ('SA', 'Q'),
+ ('SA', 'NSW'),
+ ('SA', 'V'),
+ ('NSW', 'Q'),
+ ('NSW', 'V')]
 
-for variable in variables:
-    problem.addVariable(variable, values)
 
-# Define the constraints (adjacent regions cannot have the same color)
-problem.addConstraint(lambda a, b: a != b, ('WA', 'NT'))
-problem.addConstraint(lambda a, b: a != b, ('WA', 'SA'))
-problem.addConstraint(lambda a, b: a != b, ('NT', 'SA'))
-problem.addConstraint(lambda a, b: a != b, ('NT', 'Q'))
-problem.addConstraint(lambda a, b: a != b, ('SA', 'Q'))
-problem.addConstraint(lambda a, b: a != b, ('SA', 'NSW'))
-problem.addConstraint(lambda a, b: a != b, ('SA', 'V'))
-problem.addConstraint(lambda a, b: a != b, ('NSW', 'Q'))
-problem.addConstraint(lambda a, b: a != b, ('NSW', 'V'))
+problem = Problem(variables,domains,constraints)
 
-problem = problem(variable,values,constraints)
 
 # Solve the problem
-solutions = problem.getSolutions()
+solution = problem.solve()
+print(solution)
 
-# Print the solutions (each solution is a dictionary of variable-value pairs)
-for solution in solutions:
-    print(solution)
+
--- solver.py
+++ solver.py
@@ -2,10 +2,15 @@ class Problem:
     def __init__(self, variables, domains, constraints):
         self.variables = variables
         self.domains = domains
-        self.constraints = constraints
+        self.constraints = constraints #how list ?
+
+    def is_satisfied (self,constraint,assignments): # given constrains in pairs should never be equal
+
+    #constriant representation?
+        return True
 
     def solve(self):
-        assignments = {}
+        assignments = {}# canot represented by nCr,must be given
         self.backtrack(assignments)
         return assignments
 
@@ -13,14 +18,14 @@ class Problem:
         if len(assignments) == len(self.variables):
             return True
 
-        variable = self.select_unassigned_variable(assignments)
+        variable = self.select_unassigned_variable(assignments)#find the unassigned var
 
         for value in self.order_domain_values(variable, assignments):
             if self.is_consistent(variable, value, assignments):
                 assignments[variable] = value
                 if self.backtrack(assignments):
                     return True
-                del assignments[variable]
+            del assignments[variable] #?
 
         return False
 
@@ -29,16 +34,16 @@ class Problem:
             if variable not in assignments:
                 return variable
 
-    def order_domain_values(self, variable, assignments):
+    def order_domain_values(self, variable, assignments):#domain should be changed automatically 
         return self.domains[variable]
 
     def is_consistent(self, variable, value, assignments):
         assignments[variable] = value
-        for constraint in self.constraints[variable]:
-            if not constraint.is_satisfied(assignments):
-                del assignments[variable]
+        for constraint in self.constraints:
+            if not self.is_satisfied(constraint,assignments):
+                del assignments[variable]#?
                 return False
-        del assignments[variable]
+        del assignments[variable]#?
         return True
 
 
