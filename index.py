45e0477d8c013295eb26ca8a5a641043032b6dd8
HazemShehawy <Hazemk537@gmail.com>
2023-05-24 22:40:29 +0300

    v1

--- index.py
+++ index.py
@@ -1,26 +1,22 @@
-from solver import *
+from solver import Problem
 # Define the problem
 # Define the variables (regions) and their possible values (colors)
-variables = ['WA', 'NT', 'SA', 'Q', 'NSW', 'V'] #Australia Regions
-#https://2.bp.blogspot.com/-9mXaXVUbPHQ/T8CYBASTQ4I/AAAAAAAACIo/cW09mus4IEg/s760/australia-map-picture.gif
+#MAP:::https://www.worldmap1.com/map-australia.asp
+variables = ['WA', 'NT', 'SA', 'Q', 'NSW', 'V']  # Australia Regions
 domains = ['r', 'g', 'b']
-constraints=[
- ('WA', 'NT'),
- ('WA', 'SA'),
- ('NT', 'SA'),
- ('NT', 'Q'),
- ('SA', 'Q'),
- ('SA', 'NSW'),
- ('SA', 'V'),
- ('NSW', 'Q'),
- ('NSW', 'V')]
+constraints = [
+    ('WA', 'NT'),
+    ('WA', 'SA'),
+    ('NT', 'SA'),
+    ('NT', 'Q'),
+    ('SA', 'Q'),
+    ('SA', 'NSW'),
+    ('SA', 'V'),
+    ('NSW', 'Q'),
+    ('NSW', 'V')]
 
-
-problem = Problem(variables,domains,constraints)
-
+problem = Problem(variables, domains, constraints)
 
 # Solve the problem
 solution = problem.solve()
 print(solution)
-
-
